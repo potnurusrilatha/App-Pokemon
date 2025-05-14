d163d5f4065be003e54f6e2790fceeaecdaaaf0a
srilathapotnuru <potnuru.srilatha1241@gmail.com>
2025-05-14 14:58:36 +0200

    added types page

--- src/app/types/page.tsx
+++ src/app/types/page.tsx
@@ -1,10 +1,29 @@
-import React from 'react'
+'use client'
+import './types.scss'
+  let allTypes:string []; 
 
 const Types = () => {
+  const fetchTypes = async () => {
+    try {
+    const response = await fetch('https://pokeapi.co/api/v2/type')
+    // console.log(response)
+    const data = await response.json();
+    allTypes = data.results.map((item:{ name: any; }) => item.name);
+
+    }catch (error) {
+      console.log(`Something went wrong: ${error}`)
+
+    }
+   
+  }
+  fetchTypes();
+
   return (
-    <div>
-      <p>Welcome to Types page</p>
+    <>
+    <div className="pokemon__types">
+      {allTypes && allTypes.filter((item: string, index: number) => index < 18).map((item:string) => <a href="" className={`pokemon__content--${item}`}>{item}</a>)}
     </div>
+    </>
   )
 }
 
